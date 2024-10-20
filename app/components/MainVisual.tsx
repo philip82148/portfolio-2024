347512b4516ca92f5b52b4f41becf28aea0f684c
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-10-21 01:01:17 +0900

    add FloatingTooltip

--- app/components/MainVisual.tsx
+++ app/components/MainVisual.tsx
@@ -1,37 +1,7 @@
-import { useRef, useState } from "react";
+import clsx from "clsx";
+import { useEffect, useRef, useState } from "react";
 
 export const MainVisual: React.FC = () => {
-  const timeoutId = useRef<NodeJS.Timeout>();
-  const [tooltipIsOpen, setTooltipIsOpen] = useState<boolean>(false);
-  const [tooltipProps, setTooltipProps] = useState<Omit<TooltipProps, "open">>({});
-
-  const openTooltip = (props: Omit<TooltipProps, "open">) => {
-    if (timeoutId.current) clearTimeout(timeoutId.current);
-    if (tooltipIsOpen) {
-      // close & open
-      timeoutId.current = setTimeout(() => {
-        setTooltipIsOpen(false);
-        timeoutId.current = setTimeout(() => {
-          setTooltipIsOpen(true);
-          setTooltipProps(props);
-        }, 300);
-      }, 100);
-    } else {
-      setTooltipIsOpen(true);
-      setTooltipProps(props);
-    }
-  };
-  const closeTooltip = () => {
-    if (timeoutId.current) clearTimeout(timeoutId.current);
-    timeoutId.current = setTimeout(() => {
-      setTooltipIsOpen(false);
-    }, 500);
-  };
-  const closeTooltipImmediately = () => {
-    if (timeoutId.current) clearTimeout(timeoutId.current);
-    setTooltipIsOpen(false);
-  };
-
   return (
     <div className="flex items-center justify-center h-screen w-screen min-h-[600px]">
       <div className="container flex justify-around">
@@ -42,23 +12,14 @@ export const MainVisual: React.FC = () => {
           <Skills
             title="Frontend"
             skills={["nextjs", "remix", "react", "materialui", "tailwind", "daisyui"]}
-            openTooltip={openTooltip}
-            closeTooltip={closeTooltip}
-            closeTooltipImmediately={closeTooltipImmediately}
           />
           <Skills
             title="Backend"
             skills={["nestjs", "hono", "prisma", "graphql", "grpc", "postgres", "mysql"]}
-            openTooltip={openTooltip}
-            closeTooltip={closeTooltip}
-            closeTooltipImmediately={closeTooltipImmediately}
           />
           <Skills
             title="Other Web Technologies"
             skills={["ts", "js", "nodejs", "docker", "php", "jquery", "wordpress"]}
-            openTooltip={openTooltip}
-            closeTooltip={closeTooltip}
-            closeTooltipImmediately={closeTooltipImmediately}
           />
           <Skills
             title="Other"
@@ -75,41 +36,8 @@ export const MainVisual: React.FC = () => {
               "ubuntu",
               "raspberrypi",
             ]}
-            openTooltip={openTooltip}
-            closeTooltip={closeTooltip}
-            closeTooltipImmediately={closeTooltipImmediately}
           />
         </div>
-      </div>
-      <Tooltip open={tooltipIsOpen} {...tooltipProps} />
-    </div>
-  );
-};
-
-type TooltipProps = {
-  open: boolean;
-  top?: number;
-  height?: number;
-  left?: number;
-  width?: number;
-  text?: string;
-};
-
-const Tooltip: React.FC<TooltipProps> = ({ open, top = 0, height = 0, left, text }) => {
-  const transitions = {
-    0: "opacity-0",
-    1: "",
-  };
-
-  return (
-    <div
-      className={`${
-        transitions[open ? 1 : 0]
-      } transition-opacity duration-300 absolute w-20 flex justify-center`}
-      style={{ top: top + height, left }}
-    >
-      <div className="px-2 py-1 rounded-[0.25rem] bg-slate-700 text-white text-sm mt-0.5">
-        {text}
       </div>
     </div>
   );
@@ -118,17 +46,18 @@ const Tooltip: React.FC<TooltipProps> = ({ open, top = 0, height = 0, left, text
 const Skills: React.FC<{
   title: string;
   skills: string[];
-  openTooltip: (props: Omit<TooltipProps, "open">) => void;
-  closeTooltip: () => void;
-  closeTooltipImmediately: () => void;
-}> = ({ title, skills, openTooltip, closeTooltip, closeTooltipImmediately }) => {
+}> = ({ title, skills }) => {
   // TODO: Separate icons and turn them into local svg.
   const scrollableDiv = useRef<HTMLDivElement>(null);
+  const addScrollEventListener = (listener: () => void) => {
+    scrollableDiv.current?.addEventListener("scroll", listener);
+  };
+  const getScrollLeft = () => scrollableDiv.current?.scrollLeft ?? 0;
 
   return (
     <div className="flex flex-col gap-1">
       <p className="font-bold text-2xl">{title}</p>
-      <div ref={scrollableDiv} className="overflow-x-auto" onScroll={closeTooltipImmediately}>
+      <div ref={scrollableDiv} className="overflow-x-auto">
         <div
           className="flex gap-3 w-fit"
           style={{
@@ -140,24 +69,50 @@ const Skills: React.FC<{
           }}
         >
           {skills.map((skill) => (
-            <div
+            <FloatingTooltip
               key={skill}
-              className="size-20 relative"
-              onMouseEnter={(e) => {
-                const elem = e.target as HTMLDivElement;
-
-                openTooltip({
-                  top: elem.offsetTop,
-                  height: elem.offsetHeight,
-                  left: elem.offsetLeft - (scrollableDiv.current?.scrollLeft ?? 0),
-                  width: elem.offsetWidth,
-                  text: skill,
-                });
-              }}
-              onMouseLeave={closeTooltip}
-            ></div>
+              text={skill}
+              addScrollEventListener={addScrollEventListener}
+              getScrollLeft={getScrollLeft}
+            />
           ))}
         </div>
+      </div>
+    </div>
+  );
+};
+
+// This component floats outside of the block formatting context (BFC).
+const FloatingTooltip: React.FC<{
+  text: string;
+  addScrollEventListener: (listener: () => void) => void;
+  getScrollLeft: () => number;
+}> = ({ text, addScrollEventListener, getScrollLeft }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const hoverAreaDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    addScrollEventListener(() => setIsOpen(false));
+  }, [addScrollEventListener]);
+
+  return (
+    <div
+      ref={hoverAreaDiv}
+      className="size-20"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      <div
+        className="absolute pt-0.5"
+        style={{
+          top: (hoverAreaDiv.current?.offsetTop ?? 0) + (hoverAreaDiv.current?.offsetHeight ?? 0),
+          left: (hoverAreaDiv.current?.offsetLeft ?? 0) - getScrollLeft(),
+        }}
+      >
+        <div
+          className={clsx("tooltip w-20 after:hidden mt-8", isOpen && "tooltip-open")}
+          data-tip={text}
+        />
       </div>
     </div>
   );
