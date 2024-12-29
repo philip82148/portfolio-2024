284973e20018b00ba3e73d57c90520e76d6c2fbc
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-12-29 15:17:37 +0900

    improve Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header: React.FC = () => {
   return (
     <div className="h-16">
-      <header className="fixed w-full flex justify-between h-16 px-6">
+      <header className="fixed z-50 w-full flex justify-between h-16 px-6 bg-white shadow-sm">
         <div className="flex items-center gap-2">
           <div className="rounded-full bg-primary-content size-10" />
           <div className="text-2xl font-bold">Ryota Sasaki</div>
