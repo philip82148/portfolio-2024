5d57e99f16f055e0ba6c6fa6534a72a842e7deed
philip82148 <92205270+philip82148@users.noreply.github.com>
2025-01-12 17:45:33 +0900

    improve Header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
   return (
     <header className="navbar bg-inherit sticky top-0 z-50 shadow-sm">
       <div className="flex-1">
-        <div className="btn btn-ghost text-xl">Ryota Sasaki</div>
+        <div className="flex items-center text-xl h-12 px-4 font-semibold">Ryota Sasaki</div>
       </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
@@ -14,14 +14,14 @@ export const Header: React.FC = () => {
             <TbLanguageHiragana size={18} title="Change Language" />
             <IoIosArrowDown size={8} />
           </div>
-          <ul className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <ul className="dropdown-content menu gap-0.5 bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
             <li>
-              <Link to="/ja" preventScrollReset>
+              <Link to="/ja" preventScrollReset viewTransition>
                 日本語
               </Link>
             </li>
             <li>
-              <Link to="/en" preventScrollReset>
+              <Link to="/en" preventScrollReset viewTransition>
                 English
               </Link>
             </li>
