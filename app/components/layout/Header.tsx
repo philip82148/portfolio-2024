9f39b9b880cb22458726786f3ae2c84cc4f5ce28
philip82148 <92205270+philip82148@users.noreply.github.com>
2025-01-15 18:39:24 +0900

    improve Language button

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
-import { TbLanguageHiragana } from "react-icons/tb";
+import { TbWorld } from "react-icons/tb";
 
 import { useLang, type Language } from "~/multilingual";
 
@@ -18,15 +18,16 @@ export const Header: React.FC = () => {
   return (
     <header className="navbar bg-inherit sticky top-0 z-50 shadow-sm">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">
+        <Link to={`/${serverLang}`} className="btn btn-ghost text-xl">
           Ryota Sasaki
         </Link>
       </div>
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
+        <div className="dropdown dropdown-end" title="Change language">
           <div tabIndex={0} role="button" className="btn btn-ghost">
-            <TbLanguageHiragana size={18} title="Change Language" />
-            <IoIosArrowDown size={8} />
+            <TbWorld size={16} className="mt-0.5 -mr-0.5" />
+            <span>Language</span>
+            <IoIosArrowDown size={10} />
           </div>
           <ul className="dropdown-content menu gap-1 bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
             <li>
